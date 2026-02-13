4accd34d590b70103ac92d5010274c8371e0fdf9
Trigletka <130542661+Trigletka@users.noreply.github.com>
2026-02-13 12:40:13 +0300

    Update aiogram_run.py

--- aiogram_run.py
+++ aiogram_run.py
@@ -7,35 +7,26 @@ from hendlers.start import router
 import asyncio
 
 
-# Функция для установки командного меню для бота
+# меню
 async def set_commands():
-    # Создаем список команд, которые будут доступны пользователям
     commands_tg = [BotCommand(command='start', description='Старт')]
-    # Устанавливаем эти команды как дефолтные для всех пользователей
     await bot.set_my_commands(commands_tg, BotCommandScopeDefault())
 
 
-# Функция, которая будет вызвана при запуске бота
+# Функция при запуске
 async def on_startup() -> None:
-    # Устанавливаем командное меню
     await set_commands()
-    # Устанавливаем вебхук для приема сообщений через заданный URL
     await bot.set_webhook(f"{BASE_URL}{WEBHOOK_PATH}")
-    # Отправляем сообщение администратору о том, что бот был запущен
     await bot.send_message(chat_id=ADMIN_ID, text='Бот запущен!')
 
 
-# Функция, которая будет вызвана при остановке бота
+# остановка
 async def on_shutdown() -> None:
-    # Отправляем сообщение администратору о том, что бот был остановлен
     await bot.send_message(chat_id=ADMIN_ID, text='Бот остановлен!')
-    # Удаляем вебхук и, при необходимости, очищаем ожидающие обновления
     await bot.delete_webhook(drop_pending_updates=True)
-    # Закрываем сессию бота, освобождая ресурсы
     await bot.session.close()
 
 async def start_telegram():
-    # подключаем роутер
     dp.include_router(router)
     dp.startup.register(on_startup)
     dp.shutdown.register(on_shutdown)
@@ -58,7 +49,7 @@ async def start_telegram():
     while True:
         await asyncio.sleep(3600)
 
-# Основная функция, которая запускает приложение
+# запуск
 async def main():
     telegram_task = asyncio.create_task(start_telegram())
     discord_task = asyncio.create_task(botDS.start(DS_TOKEN))
